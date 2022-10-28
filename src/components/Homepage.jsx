68b304997f2cb024880f4b1501870538b9a708c7
Evelyn Ita <evelyn.ita@wemabank.com>
2022-10-28 10:54:42 +0100

    made update to pages

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,67 +5,54 @@ import Slack from "../images/slack.png";
 import Zuri from "../images/zuri_logo.png";
 import Ingressive from "../images/I4G.png";
 
-import Button from "./Button";
 
 const Homepage = () => {
   return (
-    <div className="w-full relative">
-      <div className="max-w-4xl m-auto">
+    <>
+      <section id="profile__img" className="max-w-4xl m-auto">
         <div className="icon"></div>
-      </div>
-      <div className="flex flex-col items-center mt-">
+        <header className="flex flex-col items-center mt-">
         <img className="w-32 h-32 object-contain rounded-full" src={Evelyn} alt="My profile pics" />
-        <h2 className="font-bold text-xl mt-5">Evelyn Ita</h2>
-      </div>
+        <h2 id="slack" className="font-bold text-xl mt-5">Evelyn Ita</h2>
+      </header>
+      </section>
 
-      <div className="flex flex-col max-w-6xl m-auto mt-8 p-4">
-        <Button className=" w-full text-lg text-center">
-          <a href="https://twitter.com/EveIta_?t=DAKKnqVXaO08InuEsrwfRg&s=09" target="blank">
+      <section id= "links" className="flex flex-col w-full max-w-6xl m-auto mt-3 p-4">
+          <a id="twitter" className="buttons" href="https://twitter.com/EveIta_?t=DAKKnqVXaO08InuEsrwfRg&s=09" target="blank" rel="noopener noreferrer">
             Twitter Link
           </a>
-        </Button>
-        <Button id="btn_zuri" className=" w-full text-lg text-center">
-          <a href="https://training.zuri.team/" target="blank">
+        <a id="btn__zuri" className="buttons" href="https://training.zuri.team/" target="blank" rel="noopener noreferrer">
             Zuri Team
           </a>
-        </Button>
-        <Button id="books" className=" w-full text-lg text-center">
-          <a href="https://books.zuri.team/" target="blank">
+        <a id="books" className="buttons" href="https://books.zuri.team/" target="blank" rel="noopener noreferrer">
             Zuri Books
           </a>
-        </Button>
-        <Button id="books_python" className=" w-full text-lg text-center">
-          <a href="https://books.zuri.team/python-for-beginners?ref_id=eveita" target="blank">
+        <a id="book__python" className="buttons" href="https://books.zuri.team/python-for-beginners?ref_id=eveita" target="blank" rel="noopener noreferrer">
             Python Books
           </a>
-        </Button>
-        <Button id="pitch" className=" w-full text-lg text-center">
-          <a href="https://background.zuri/team/" target="blank">
+        <a id="pitch" className="buttons" href="https://background.zuri/team/" target="blank" rel="noopener noreferrer">
             Background Check for Coders
           </a>
-        </Button>
-        <Button id="book_design" className=" w-full text-lg text-center">
-          <a href="https://books.zuri.team/design-rules/" target="blank">
+        <a id="book__design" className="buttons" href="https://books.zuri.team/design-rules/" target="blank" rel="noopener noreferrer">
             Design Books
           </a>
-        </Button>
-      </div>
+      </section>
 
-      <div className="flex justify-center mt-10 mb-20 gap-7">
-        <a href="https://app.slack.com/client/T042F7V19Q8/C041JU70S5U/rimeto_profile/U0490F6JDU0" target="blank">
+      <section id="social-links" className="flex justify-center mt-10 mb-20 gap-7">
+        <a id="slack" className="hidden" href="https://app.slack.com/client/T042F7V19Q8/C041JU70S5U/rimeto_profile/U0490F6JDU0" target="blank" rel="noopener noreferrer">
           <img src={Slack} alt="" />
         </a>
-        <a href="https://github.com/yvehogan" target="blank">
+        <a id="github" href="https://github.com/yvehogan" target="blank" rel="noopener noreferrer" >
           <img src={Github} alt="" />
         </a>
-      </div>
+      </section>
 
-      <footer className="max-w-7xl flex lg:flex-row flex-col justify-between p-8 m-auto border-t border-primary">
+      <section id="footer" className="max-w-7xl flex lg:flex-row flex-col justify-between p-8 m-auto border-t border-primary">
         <img className="lg:w-auto w-32" src={Zuri} alt="" />
         <p className="lg:mt-0 mt-4 lg:mb-0 mb-4">HNG Internship ( Frontend Task</p>
         <img className="lg:w-auto w-32" src={Ingressive} alt="" />
-      </footer>
-    </div>
+      </section>
+    </>
   );
 };
 
